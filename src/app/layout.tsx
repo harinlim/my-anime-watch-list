bb6db8b9d30b62d706b8f7eaaa7a0d39783d8f84
Harin Lim <hlim@unc.edu>
2024-05-09 02:02:07 -0400

    ci: remove favicon reference

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default async function RootLayout({
     <html lang="en">
       <head>
         <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        {/* <link rel="shortcut icon" href="/favicon.svg" /> */}
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
