3822bf3e7431576c17b577e749dce7677378dbff
Harin Lim <hlim@unc.edu>
2024-05-09 19:52:45 -0400

    feat: add `DELETE` for watchlist anime

--- src/app/api/watchlists/[watchlistId]/route.ts
+++ src/app/api/watchlists/[watchlistId]/route.ts
@@ -49,7 +49,7 @@ export async function GET(_: NextRequest, { params }: RouteParams) {
 
 /**
  * Add an anime to an existing watchlist.
- * Expects body:
+ * Body:
  *  - animeId: string | number
  */
 export async function POST(request: NextRequest, { params }: RouteParams) {
@@ -134,4 +134,70 @@ export async function POST(request: NextRequest, { params }: RouteParams) {
   }
 
   return NextResponse.json('Anime added to watchlist', { status: 201 })
+}
+
+/**
+ * Remove an anime from a watchlist.
+ * Body:
+ *  - animeId: string | number
+ */
+export async function DELETE(request: NextRequest, { params }: RouteParams) {
+  const watchlistId = Number(params.watchlistId)
+  if (Number.isNaN(watchlistId) || watchlistId <= 0) {
+    return NextResponse.json('Invalid watchlist ID', { status: 400 })
+  }
+
+  const body = await parseRequestBody(request, watchlistAnimeRequestBodySchema)
+  if (body === null) {
+    return NextResponse.json('Failed to parse request body', { status: 400 })
+  }
+
+  const animeId = `${body.animeId}`
+
+  const supabase = createServerClient()
+  // Check if a user's logged in
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (!user) {
+    return NextResponse.json('Failed authorization', { status: 401 })
+  }
+
+  const watchlistQuery = await supabase
+    .from('watchlists')
+    .select('*')
+    .eq('id', watchlistId)
+    .single()
+
+  // Verify the watchlist belonging to the user exists
+  if (!!watchlistQuery.error || !watchlistQuery.data) {
+    if (watchlistQuery.status === 406) {
+      return NextResponse.json('Watchlist not found', { status: 404 })
+    }
+
+    return NextResponse.json('Failed to fetch watchlist', { status: watchlistQuery.status })
+  }
+
+  const watchlist = watchlistQuery.data
+  if (watchlist.user_id !== user.id) {
+    return NextResponse.json('Watchlist does not belong to the user', { status: 403 })
+  }
+
+  // Remove anime from watchlist
+  const deleteQuery = await supabase
+    .from('watchlists_anime')
+    .delete({ count: 'exact' })
+    .eq('watchlist_id', watchlistId)
+    .eq('anime_id', animeId)
+
+  if (deleteQuery.error) {
+    return NextResponse.json(deleteQuery.error, { status: deleteQuery.status })
+  }
+
+  if (!deleteQuery.count) {
+    return NextResponse.json('Anime not found in watchlist', { status: 404 })
+  }
+
+  return NextResponse.json(null, { status: 204 })
 }
