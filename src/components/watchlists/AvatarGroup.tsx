2789b60398617e4889b24948a9a7f9be07a16eb4
Harin Lim <hlim@unc.edu>
2024-05-18 11:14:47 -0400

    perf: improve render performance on rows and opt out profile prefetch

--- src/components/watchlists/AvatarGroup.tsx
+++ src/components/watchlists/AvatarGroup.tsx
@@ -7,61 +7,66 @@ import styles from './AvatarGroup.module.css'
 
 import type { WatchlistOverview } from '@/types/watchlists'
 
-type WatchlistUsers = WatchlistOverview['watchlists_users']
+type WatchlistUser = WatchlistOverview['watchlists_users'][number]
 
-type AvatarGroupProps = {
-  watchlist_users: WatchlistUsers
+function Avatar({ user }: { user: WatchlistUser }) {
+  const a11yLabel = `${user.role}: ${user.username}`
+
+  return (
+    <Tooltip key={user.user_id} label={`@${user.username}`} position="top">
+      <Link href={`/profile/${user.username}`} prefetch={false}>
+        {user.avatar_url ? (
+          <Image
+            key={user.user_id}
+            className={clsx(styles.ring, 'inline-block')}
+            src={
+              user.avatar_url ??
+              'https://t4.ftcdn.net/jpg/03/40/12/49/240_F_340124934_bz3pQTLrdFpH92ekknuaTHy8JuXgG7fi.jpg'
+            }
+            alt={a11yLabel}
+          />
+        ) : (
+          <svg
+            key={user.user_id}
+            className={clsx(
+              styles.ring,
+              'inline-block h-8 w-8 rounded-full bg-zinc-300 text-gray-400 dark:bg-zinc-500'
+            )}
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            aria-label={a11yLabel}
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+              clipRule="evenodd"
+            />
+          </svg>
+        )}
+      </Link>
+    </Tooltip>
+  )
 }
 
-export function AvatarGroup({ watchlist_users }: AvatarGroupProps) {
-  const watchlistUsersOverview = watchlist_users.slice(0, 4)
+export function AvatarGroup({ watchlist_users }: { watchlist_users: WatchlistUser[] }) {
+  const watchlistUsersOverview = watchlist_users.slice(0, 2)
   const remainingUsers = watchlist_users.length - watchlistUsersOverview.length
 
   return (
     <div className="flex -space-x-2">
       {watchlistUsersOverview.map(user => (
-        <Tooltip key={user.user_id} label={`@${user.username}`} position="top">
-          <Link href={`/profile/${user.username}`}>
-            {user.avatar_url ? (
-              <Image
-                key={user.user_id}
-                className={clsx(styles.ring, 'inline-block')}
-                src={
-                  user.avatar_url ??
-                  'https://t4.ftcdn.net/jpg/03/40/12/49/240_F_340124934_bz3pQTLrdFpH92ekknuaTHy8JuXgG7fi.jpg'
-                }
-                alt={`${user.username} avatar`}
-              />
-            ) : (
-              <svg
-                key={user.user_id}
-                className={clsx(
-                  styles.ring,
-                  'inline-block bg-zinc-300 text-gray-400 dark:bg-zinc-500'
-                )}
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                aria-label={`${user.username} avatar`}
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            )}
-          </Link>
-        </Tooltip>
+        <Avatar user={user} />
       ))}
       {remainingUsers > 0 && (
         <span
+          aria-label={`And ${remainingUsers} more contributors`}
           className={clsx(
             styles.ring,
-            'inline-flex items-center justify-center bg-gray-100 dark:bg-neutral-600'
+            'inline-flex h-8 w-8 items-center justify-center rounded-full bg-gray-100 dark:bg-neutral-600'
           )}
         >
-          <span className="font-medium leading-none text-gray-500 dark:text-neutral-400">
+          <span aria-hidden className="text-sm leading-none text-gray-600 dark:text-neutral-200">
             {remainingUsers}+
           </span>
         </span>
--- src/components/watchlists/WatchlistRow.tsx
+++ src/components/watchlists/WatchlistRow.tsx
@@ -1,7 +1,8 @@
-import { Text, Paper, Flex, Box, Group } from '@mantine/core'
+import { Text, Paper, Flex, Group, Box } from '@mantine/core'
 import { IconEye, IconLock } from '@tabler/icons-react'
 import clsx from 'clsx'
 import Link from 'next/link'
+import { memo } from 'react'
 
 import { AvatarGroup } from './AvatarGroup'
 import { WatchlistCoverPhoto } from './WatchlistCoverPhoto'
@@ -13,36 +14,36 @@ type Props = {
   watchlist: WatchlistOverview
 }
 
-export function WatchlistRow({ watchlist }: Props) {
-  return (
-    <Paper shadow="lg" radius="md" p="lg" className={clsx(styles.row, 'h-58 w-full')}>
-      <Box className="flex h-full flex-col justify-between">
-        <Flex>
-          <div className="size-32 shrink-0">
-            <WatchlistCoverPhoto watchlist={watchlist} hasPlus={false} />
-          </div>
-          <Flex direction="column" className="ml-4">
-            <Link href={`/watchlists/${watchlist.id}`} className="hover:underline">
-              <Text className="line-clamp-1 text-lg font-bold md:text-xl">{watchlist.title}</Text>
-            </Link>
-            <Text className="line-clamp-3">{watchlist.description}</Text>
-          </Flex>
+export const WatchlistRow = memo(({ watchlist }: Props) => (
+  <Paper shadow="lg" radius="md" p="lg" className={clsx(styles.row, 'h-58 w-full')}>
+    <Flex className="h-full flex-col justify-between">
+      <Flex>
+        <Box className="size-32 shrink-0">
+          <WatchlistCoverPhoto watchlist={watchlist} hasPlus={false} />
+        </Box>
+        <Flex direction="column" className="ml-4">
+          <Link href={`/watchlists/${watchlist.id}`} className="hover:underline">
+            <Text className="line-clamp-1 text-lg font-bold md:text-xl">{watchlist.title}</Text>
+          </Link>
+          <Text className="line-clamp-3 text-base">{watchlist.description}</Text>
         </Flex>
-        <Flex className="items-end justify-between">
-          {watchlist.is_public ? (
-            <Group className="items-center gap-2 opacity-80">
-              <IconEye color="rgb(22 163 74)" stroke="1" size="20" />
-              <Text className="text-green-600">Public</Text>
-            </Group>
-          ) : (
-            <Group className="items-center gap-1 opacity-80">
-              <IconLock stroke="1" size="20" />
-              <Text>Private</Text>
-            </Group>
-          )}
-          <AvatarGroup watchlist_users={watchlist.watchlists_users} />
-        </Flex>
-      </Box>
-    </Paper>
-  )
-}
+      </Flex>
+
+      <Flex className="items-end justify-between">
+        {watchlist.is_public ? (
+          <Group className="items-center gap-1.5 opacity-80">
+            <IconEye color="rgb(22 163 74)" stroke="1" size="20" />
+            <Text className="text-sm text-green-600">Public</Text>
+          </Group>
+        ) : (
+          <Group className="items-center gap-1.5 opacity-80">
+            <IconLock stroke="1" size="18" />
+            <Text className="text-sm">Private</Text>
+          </Group>
+        )}
+
+        <AvatarGroup watchlist_users={watchlist.watchlists_users} />
+      </Flex>
+    </Flex>
+  </Paper>
+))
