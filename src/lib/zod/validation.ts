4ed75ff00cd61b58cc1c7af3fb346a2139958f8a
Harin Lim <hlim@unc.edu>
2024-05-25 17:02:02 -0400

    fix: use zod field errors for validation error responses

--- src/lib/zod/validation.ts
+++ src/lib/zod/validation.ts
@@ -1,8 +1,7 @@
 import type { ZodError } from 'zod'
 
-// TODO: make this more api-consumer friendly
 export function transformZodValidationErrorToResponse(error: ZodError) {
   return {
-    errors: error.issues,
+    errors: error.flatten().fieldErrors,
   }
 }
