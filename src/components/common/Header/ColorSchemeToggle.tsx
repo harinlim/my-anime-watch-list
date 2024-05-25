b3a47757a2f6db76f49985f05512fc2f1c7aaa82
Harin Lim <hlim@unc.edu>
2024-05-25 17:04:45 -0400

    refactor: make `ColorSchemeToggle` use tailwind instead of `light-dark`

--- src/components/common/Header/ColorSchemeToggle.tsx
+++ src/components/common/Header/ColorSchemeToggle.tsx
@@ -1,24 +1,26 @@
 'use client'
 
-import { ActionIcon, useComputedColorScheme, useMantineColorScheme } from '@mantine/core'
+import {
+  ActionIcon as ActionIconButton,
+  useComputedColorScheme,
+  useMantineColorScheme,
+} from '@mantine/core'
 import { IconMoon, IconSun } from '@tabler/icons-react'
-import cx from 'clsx'
-
-import classes from './ColorSchemeToggle.module.css'
 
 export function ColorSchemeToggle() {
   const { setColorScheme } = useMantineColorScheme()
   const computedColorScheme = useComputedColorScheme('dark', { getInitialValueInEffect: true })
 
   return (
-    <ActionIcon
+    <ActionIconButton
+      type="button"
       onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
       variant="default"
       size="lg"
       aria-label="Toggle color scheme"
     >
-      <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
-      <IconMoon className={cx(classes.icon, classes.dark)} stroke={1.5} />
-    </ActionIcon>
+      <IconSun className="hidden dark:block" stroke={1.5} />
+      <IconMoon className="dark:hidden" stroke={1.5} />
+    </ActionIconButton>
   )
 }
