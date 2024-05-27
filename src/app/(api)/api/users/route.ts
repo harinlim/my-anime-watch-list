42e4c648e1a99d55e233214e4cb928733d50e9eb
Harin Lim <hlim@unc.edu>
2024-05-26 20:29:14 -0400

    feat: add limit query param and refactor out map

--- src/app/(api)/api/users/route.ts
+++ src/app/(api)/api/users/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from 'next/server'
 
 import { createServerClient } from '@/lib/supabase/server'
+import { transformZodValidationErrorToResponse } from '@/lib/zod/validation'
 
+import { MAX_USERS_LIMIT } from './constants'
 import { searchUsersParamsSchema } from './schemas'
 
 import type { User, PublicUser } from '@/types/users'
@@ -25,15 +27,24 @@ export async function GET(request: NextRequest) {
   const { searchParams } = request.nextUrl
 
   const searchParamsResult = searchUsersParamsSchema.safeParse({
+    /* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
     search: searchParams.get('search'),
+    limit: searchParams.get('limit') || null,
+    /* eslint-enable @typescript-eslint/prefer-nullish-coalescing */
   })
 
   // If search params are invalid, return an error
   if (!searchParamsResult.success) {
-    return NextResponse.json('Invalid search params', { status: 400 })
+    return NextResponse.json(
+      {
+        message: 'Invalid search params',
+        ...transformZodValidationErrorToResponse(searchParamsResult.error),
+      },
+      { status: 400 }
+    )
   }
 
-  // If no search query is provided, return the user's information aka ME
+  // If no search query is provided, return the current user's information aka ME
   if (searchParamsResult.data.search === null) {
     const userResult = await supabase
       .from('users')
@@ -56,43 +67,37 @@ export async function GET(request: NextRequest) {
     })
   }
 
-  const searchValue = searchParamsResult.data.search
+  const { limit, search: searchValue } = searchParamsResult.data
 
   // If an empty search query is provided, return all users
   if (searchValue === '') {
-    const usersResults = await supabase
+    const userSearchResults = await supabase
       .from('users')
       .select('id, username, avatar_url')
       .order('username')
-      .limit(20)
+      .limit(limit ?? MAX_USERS_LIMIT)
 
-    if (usersResults.error) {
-      console.error(usersResults.error)
+    if (userSearchResults.error) {
+      console.error(userSearchResults.error)
       return NextResponse.json('Failed to fetch users', { status: 500 })
     }
 
-    return NextResponse.json<PublicUser[]>(usersResults.data, { status: 200 })
+    return NextResponse.json<PublicUser[]>(userSearchResults.data, { status: 200 })
   }
 
   // If a search query is provided, return all users that have usernames starting with the search query
   // RPC is ordered by length of username, then by username (alphabetically)
-  const usersResults = await supabase
+  const userSearchResults = await supabase
     .rpc('search_users_by_username_prefix', {
       prefix: searchValue,
     })
-    .limit(20)
+    .select('id, username, avatar_url')
+    .limit(limit ?? MAX_USERS_LIMIT)
 
-  if (usersResults.error) {
-    console.error(usersResults.error)
+  if (userSearchResults.error) {
+    console.error(userSearchResults)
     return NextResponse.json('Failed to fetch users', { status: 500 })
   }
 
-  return NextResponse.json<PublicUser[]>(
-    usersResults.data.map(({ id, username, avatar_url }) => ({
-      id,
-      username,
-      avatar_url,
-    })),
-    { status: 200 }
-  )
+  return NextResponse.json<PublicUser[]>(userSearchResults.data, { status: 200 })
 }
--- src/app/(api)/api/users/schemas.ts
+++ src/app/(api)/api/users/schemas.ts
@@ -1,5 +1,15 @@
 import { z } from 'zod'
 
-export const searchUsersParamsSchema = z.object({
-  search: z.string().nullable(),
-})
+import { MAX_USERS_LIMIT } from './constants'
+
+export const searchUsersParamsSchema = z
+  .object({
+    search: z.string(),
+    limit: z.number().int().min(1).max(MAX_USERS_LIMIT).nullable(),
+  })
+  .or(
+    z.object({
+      search: z.null(),
+      limit: z.null(),
+    })
+  )
