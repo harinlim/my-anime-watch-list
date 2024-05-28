f3609549bc6afd508171bdc2fc3dbad1027eb2e7
Harin Lim <hlim@unc.edu>
2024-05-27 20:45:34 -0400

    feat: add privileged view on buttons

--- src/app/watchlists/[watchlistId]/EditCollaboratorsButton.tsx
+++ src/app/watchlists/[watchlistId]/EditCollaboratorsButton.tsx
@@ -1,11 +1,16 @@
 'use client'
 
-import { IconUserPlus } from '@tabler/icons-react'
+import { IconUserPlus, IconUsers } from '@tabler/icons-react'
 
+import { useCollaboratorsData } from '@/components/collaborators/CollaboratorsContext'
 import { useEditCollaboratorsModal } from '@/components/collaborators/CollaboratorsModal/CollaboratorsModalContext'
 
 export function EditCollaboratorsButton() {
   const [, { open }] = useEditCollaboratorsModal()
+  const { currentUserCollaborator } = useCollaboratorsData()
+
+  const hasEditAccess =
+    currentUserCollaborator?.role === 'owner' || currentUserCollaborator?.role === 'editor'
 
   return (
     <button
@@ -14,7 +19,7 @@ export function EditCollaboratorsButton() {
       aria-label="edit collaborators"
       className="rounded-full border-2 border-solid border-[#c9c9c9] p-1 hover:border-[#a9a9a9] hover:bg-[#f5f5f5] dark:border-[#3a3a3a] dark:hover:border-[#4a4a4a] dark:hover:bg-[#2b2b2b]"
     >
-      <IconUserPlus size={32} />
+      {hasEditAccess ? <IconUserPlus size={32} /> : <IconUsers size={32} />}
     </button>
   )
 }
--- src/app/watchlists/[watchlistId]/EditWatchlistButton.tsx
+++ src/app/watchlists/[watchlistId]/EditWatchlistButton.tsx
@@ -1,13 +1,24 @@
+'use client'
+
 import { IconEdit } from '@tabler/icons-react'
 
+import { useCollaboratorsData } from '@/components/collaborators/CollaboratorsContext'
+
 export function EditWatchlistButton() {
+  const { currentUserCollaborator } = useCollaboratorsData()
+
+  const hasEditAccess =
+    currentUserCollaborator?.role === 'owner' || currentUserCollaborator?.role === 'editor'
+
   return (
-    <button
-      type="button"
-      aria-label="edit watchlist"
-      className="rounded-full border-2 border-solid border-[#c9c9c9] p-1 hover:border-[#a9a9a9] hover:bg-[#f5f5f5] dark:border-[#3a3a3a] dark:hover:border-[#4a4a4a] dark:hover:bg-[#2b2b2b]"
-    >
-      <IconEdit size={32} />
-    </button>
+    hasEditAccess && (
+      <button
+        type="button"
+        aria-label="edit watchlist"
+        className="rounded-full border-2 border-solid border-[#c9c9c9] p-1 hover:border-[#a9a9a9] hover:bg-[#f5f5f5] dark:border-[#3a3a3a] dark:hover:border-[#4a4a4a] dark:hover:bg-[#2b2b2b]"
+      >
+        <IconEdit size={32} />
+      </button>
+    )
   )
 }
