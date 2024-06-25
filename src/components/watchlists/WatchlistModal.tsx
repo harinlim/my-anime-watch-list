6ef79f7214900439b8f4697b62e72f29bd795b6b
Harin Lim <hlim@unc.edu>
2024-06-25 12:34:22 -0400

    feat: add backlinks to watchlist in watchlist modal

--- src/components/watchlists/WatchlistModal.tsx
+++ src/components/watchlists/WatchlistModal.tsx
@@ -3,7 +3,6 @@
 import {
   Button,
   Text,
-  UnstyledButton,
   Group,
   Stack,
   ModalBody,
@@ -13,23 +12,21 @@ import {
   ModalOverlay,
   ModalRoot,
   ModalTitle,
+  ActionIcon,
+  Tooltip,
 } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
-import { IconPlus } from '@tabler/icons-react'
-import { usePathname, useRouter } from 'next/navigation'
-import { useMemo, createContext, useContext } from 'react'
+import { IconEdit, IconPlus } from '@tabler/icons-react'
+import clsx from 'clsx'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { useMemo, createContext, useContext, memo } from 'react'
 
 import { WatchlistCheckbox } from './WatchlistCheckbox'
 
 import type { Watchlist } from '@/types/watchlists'
 import type { ButtonProps } from '@mantine/core'
 import type { ReactNode } from 'react'
-
-type WatchlistModalProps = {
-  animeId: string
-  editableWatchlists: Watchlist[]
-  initialAddedWatchlists: Set<number>
-}
 
 const WatchlistModalContext = createContext<ReturnType<typeof useDisclosure> | null>(null)
 
@@ -67,35 +64,77 @@ export function OpenWatchlistModalButton({ ...rest }: ButtonProps) {
   )
 }
 
+type WatchlistRowProps = {
+  watchlist: Watchlist
+  animeId: string
+  defaultChecked: boolean
+  className?: string
+}
+
+const WatchlistRow = memo(
+  ({ watchlist, animeId, defaultChecked, className }: WatchlistRowProps) => {
+    const [isLoading, { toggle }] = useDisclosure()
+
+    return (
+      <div key={watchlist.id} className={clsx('flex items-center justify-between', className)}>
+        <div className="flex min-w-0 items-center gap-5 text-ellipsis">
+          <WatchlistCheckbox
+            id={`watchlist-${watchlist.id}:${animeId}`}
+            animeId={animeId}
+            watchlistId={watchlist.id}
+            defaultChecked={defaultChecked}
+          />
+          <Tooltip label={watchlist.title} multiline>
+            <Text
+              htmlFor={`watchlist-${watchlist.id}:${animeId}`}
+              component="label"
+              className="line-clamp-2 text-ellipsis break-words text-base"
+            >
+              {watchlist.title}
+            </Text>
+          </Tooltip>
+        </div>
+
+        <ActionIcon
+          component={Link}
+          href={`/watchlists/${watchlist.id}`}
+          aria-label="Go to watchlist"
+          variant="default"
+          className="ml-4"
+          disabled={isLoading}
+          onClick={toggle}
+        >
+          <IconEdit className="h-6 w-6" stroke={1.5} />
+        </ActionIcon>
+      </div>
+    )
+  }
+)
+
+type WatchlistModalProps = {
+  animeId: string
+  editableWatchlists: Watchlist[]
+  initialAddedWatchlists: Set<number>
+}
+
 export function WatchlistModal({
   animeId,
   editableWatchlists,
   initialAddedWatchlists,
 }: WatchlistModalProps) {
   const [isOpen, { close }] = useWatchlistModal()
-  const router = useRouter()
   const currentPath = usePathname()
 
   const content = useMemo(
     () =>
       // TODO: bubble up checked watchlists to the top
-      // TODO: add link to watchlist
       editableWatchlists.map(watchlist => (
-        <div key={watchlist.id} className="mt-4 flex items-center gap-5">
-          <WatchlistCheckbox
-            id={`watchlist-${watchlist.id}:${animeId}`}
-            animeId={animeId}
-            watchlistId={watchlist.id}
-            defaultChecked={initialAddedWatchlists.has(watchlist.id)}
-          />
-          <Text
-            htmlFor={`watchlist-${watchlist.id}:${animeId}`}
-            component="label"
-            className="line-clamp-2 text-base"
-          >
-            {watchlist.title}
-          </Text>
-        </div>
+        <WatchlistRow
+          className="mt-2 h-10"
+          watchlist={watchlist}
+          animeId={animeId}
+          defaultChecked={initialAddedWatchlists.has(watchlist.id)}
+        />
       )),
     [animeId, editableWatchlists, initialAddedWatchlists]
   )
@@ -109,19 +148,21 @@ export function WatchlistModal({
             <ModalTitle className="text-lg font-semibold">Add to watchlist</ModalTitle>
             <ModalCloseButton />
           </ModalHeader>
+
           <ModalBody component="section" className="flex shrink flex-col p-0">
-            <div className="relative max-h-64 overflow-auto px-6 pb-4 sm:max-h-80">{content}</div>
+            <div className="relative max-h-64 overflow-auto px-4 pb-4 sm:max-h-80">{content}</div>
           </ModalBody>
+
           <section className="flex w-full border-t-[1px] border-t-[--mantine-color-gray-3] bg-[--mantine-color-white] px-1 py-1 align-middle dark:border-t-[--mantine-color-dark-4] dark:bg-[--mantine-color-dark-7]">
-            <UnstyledButton
-              onClick={() => router.push(`/watchlists/create?returnURL=${currentPath}`)}
+            <Link
+              href={`/watchlists/create?returnURL=${currentPath}`}
               className="w-full rounded-sm px-4 py-3 hover:bg-[--mantine-color-gray-2] dark:hover:bg-[--mantine-color-dark-4]"
             >
               <Group component="span" className="flex-nowrap gap-1">
                 <IconPlus size={20} />
                 <Text className="ml-2 line-clamp-1">Create new watchlist</Text>
               </Group>
-            </UnstyledButton>
+            </Link>
           </section>
         </Stack>
       </ModalContent>
